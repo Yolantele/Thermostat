91f10394ac3c9521027e597b42b22c13825e8bb0
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-24 09:59:14 +0000

    code tydying

--- interface.js
+++ interface.js
@@ -2,21 +2,17 @@
 $(document).ready(function () {
   var thermostat = new Thermostat()
   function updateTemp () {
-
-    $('#temperature').text(thermostat.temperature);
-    if(thermostat.checkUsage() === 0){
+    $('#temperature').text(thermostat.temperature)
+      if(thermostat.checkUsage() === 0) {
       $('#temperature').css('color', 'green')
     } if (thermostat.checkUsage() === 1) {
       $('#temperature').css('color', 'yellow')
-    } if(thermostat.checkUsage() === 2){
+    } if (thermostat.checkUsage() === 2) {
       $('#temperature').css('color', 'red')
     }
   }
-
-
-  updateTemp();
-  switchColor();
-
+  updateTemp()
+  switchColor()
 
   $('#temperature-up').on('click', function () {
     thermostat.up()
@@ -30,28 +26,25 @@ $(document).ready(function () {
     thermostat.resets()
     updateTemp()
   })
-  $('#powersaving-switch').on('click', function() {
-    switchMode();
+  $('#powersaving-switch').on('click', function () {
+    switchMode()
   })
-
-  function switchColor() {
-    if (thermostat.powerSave == true) {
-      $('#powersaving-switch').css('background-color', 'green');
+  function switchColor () {
+    if (thermostat.powerSave === true) {
+      $('#powersaving-switch').css('background-color', 'green')
     } else {
-      $('#powersaving-switch').css('background-color', 'orange');
+      $('#powersaving-switch').css('background-color', 'orange')
     }
   }
-
-  function switchMode() {
-    thermostat.switch();
-    updateTemp();
-    switchColor();
+  function switchMode () {
+    thermostat.switch()
+    updateTemp()
+    switchColor()
   }
-
-  $('#current-city').change(function() {
-     var city = $('#current-city').val();
-     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=8e6bb843aaeecb68aa5a7a85d4d34202', function(data) {
+  $('#current-city').change(function () {
+    var city = $('#current-city').val()
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=8e6bb843aaeecb68aa5a7a85d4d34202', function(data) {
       $('#current-temperature').text(data.main.temp)
     })
   })
-});
+})
